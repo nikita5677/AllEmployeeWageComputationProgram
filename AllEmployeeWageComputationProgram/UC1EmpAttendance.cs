ab160a18e518ab5701132defa11292fc6aa7a556
nikit <nikita.nikita@gmail.com>
2022-11-26 10:18:25 +0530

    Add project files.

---
+++ AllEmployeeWageComputationProgram/UC1EmpAttendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllEmployeeWageComputationProgram
+{
+    class UC1EmpAttendance
+    {
+        public static void Attendance()
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empcheck = random.Next(0, 2);
+            if(empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
