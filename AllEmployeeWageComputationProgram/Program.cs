69a5a35dc38fb7dd1694d3328eafc24390f9d81d
nikit <nikita.nikita@gmail.com>
2022-11-26 11:43:12 +0530

    Ability to get the Total Wage when queried by Company.

--- AllEmployeeWageComputationProgram/Program.cs
+++ AllEmployeeWageComputationProgram/Program.cs
@@ -10,16 +10,13 @@ namespace AllEmployeeWageComputationProgram
     {
         static void Main(string[] args)
         {
-            //UC11
-            EmpWageArray empWageBuilder = new EmpWageArray();
-            empWageBuilder.addCompanyEmpWage("TechMahindra", 25, 7, 160);
-            empWageBuilder.addCompanyEmpWage("TripStack", 22, 10, 230);
-            empWageBuilder.addCompanyEmpWage("LG", 23, 3, 100);
-            empWageBuilder.addCompanyEmpWage("Wipro", 28, 4, 160);
-            empWageBuilder.addCompanyEmpWage("Enzigma", 15, 2, 180);
-            empWageBuilder.addCompanyEmpWage("HCL", 25, 7, 240);
-            empWageBuilder.addCompanyEmpWage("Samsung", 41, 10, 210);
+            //UC14
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("TCS", 22, 3, 15);
+            empWageBuilder.addCompanyEmpWage("Jio", 15, 3, 20);
             empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total wage for TCS company : " + empWageBuilder.getTotalWage("TCS"));
+            Console.WriteLine("Total wage for Jio company : " + empWageBuilder.getTotalWage("Jio"));
         }
     }
 }
